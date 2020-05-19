57be3bb64515a9cb3991d1e20d201ee02e1ece89
no-blyat <psudarshan3@gmail.com>
2020-05-20 03:10:17 +0530

    Second...

--- game/views.py
+++ game/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+import asyncio
+import websockets
 
 
 template = 'game/index.html'
 
+async def startGame(websocket, path):
+    move = await websocket.recv()
+    await websocket.end("hello")
+    
+
+
+
 def game(request):
+    start_server = websockets.serve(startGame, "127.0.0.1", 5678)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
     return render(request, template, {'range': range(9)})
 
 # Web Sockets, Channels, AI Bot
